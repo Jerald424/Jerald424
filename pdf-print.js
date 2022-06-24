65fbce8a2b7df808240736b155b1b47ed5d44163
Jerald424 <107866527+Jerald424@users.noreply.github.com>
2022-06-24 13:49:43 +0530

    Create pdf-print.js

---
+++ pdf-print.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Pdf from "react-to-pdf";
+const ref = React.createRef();
+export default function PrintTabeToPdf() {
+  return (
+    <div>
+      PrintTabeToPdf
+      <Pdf targetRef={ref} filename='jerald-sample.pdf'>
+        {({ toPdf }) => <button onClick={toPdf}>generate pdf</button>}
+      </Pdf>
+      <div ref={ref}>
+        <h1>hello my first pdf</h1>
+        <h2>start with to see some magic</h2>
+      </div>
+    </div>
+  );
+}
